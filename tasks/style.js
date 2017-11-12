cefb5610cd5ea8d9304440c4adacedb302853886
Unknown <dr.mihail@yandex.ru>
2017-11-12 13:16:19 +0500

    добавлен gulp-sass-inheritance

--- tasks/style.js
+++ tasks/style.js
@@ -6,15 +6,23 @@ import gulpIf from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
 import autoprefixer from 'gulp-autoprefixer';
 import rename from 'gulp-rename';
+import sassInheritance from 'gulp-sass-inheritance';
+import cached from 'gulp-cached';
+import filter from 'gulp-filter';
 
 gulp.task('style', () => {
   gulp.src('src/components/**/*.scss')
     .pipe(plumber({
       errorHandler: errorHandler(`Ошибка в \'Стилях\' task`)
     }))
+    .pipe(cached('sass'))
+    .pipe(gulpIf(global.isWatching, sassInheritance({
+      dir: 'src'
+    })))
+    .pipe(filter(file => /src[\\\/]components/.test(file.path)))
     .pipe(sourcemaps.init())
     .pipe(sass({
-      // outputStyle: 'compressed'
+      outputStyle: 'compressed'
     }))
     .pipe(autoprefixer({
       browsers: ['last 200 versions'],
